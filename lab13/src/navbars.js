d2ab0e32d982b3ef29968403ad64bb72ab2a193b
hwu <38544670+huixinwu@users.noreply.github.com>
2025-04-22 19:51:20 +0000

    complete React routing

---
+++ lab13/src/navbars.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import './App.css'
+
+
+const Navbars = function(){
+    return(
+        <>
+            <nav className="navheader">
+                <section className="iconlink">
+                    <a href="https://www.qcc.cuny.edu" target="_blank">QCC</a>
+                </section>
+                <section className="navlinks">
+                    <button className="tablink"><Link to="/">Home</Link></button>
+                    <button className="tablink"><Link to="/about">About Us</Link></button>
+                    <button className="tablink"><Link to="/contact">Contact Us</Link></button>
+                </section>
+            </nav>
+        </>
+    )
+}
+
+export default Navbars
